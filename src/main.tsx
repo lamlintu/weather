538e5b41917c56d0cd3f0268d9f189e16347e162
Lin Lam <lin.lam@danfoss.com>
2026-05-09 22:04:02 +0300

    Add global flex grid styles utilities

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/main.scss";
+import "./styles/flex-grid.scss";
 import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
