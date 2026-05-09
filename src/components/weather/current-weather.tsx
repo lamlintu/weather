f8f6e2087e54f5b82dca2d16caff5e4e301a50e4
Lin Lam <lin.lam@danfoss.com>
2026-05-09 12:57:20 +0300

    Init styles for CurrentWeather
    - Still hard-coded values.

--- src/components/weather/current-weather.tsx
+++ src/components/weather/current-weather.tsx
@@ -1,8 +1,21 @@
+import s from "./current-weather.module.scss";
+
 export function CurrentWeather() {
   return (
-    <div>
-      <h1>Helsinki</h1>
-      <p>15 °C</p>
+    <div className={s.container}>
+      <article>
+        <h2 className={s.title}>Helsinki</h2>
+        <p className={s.temp}>15 °C</p>
+      </article>
+
+      <div className={s.icon}>
+        <img
+          src="src/assets/icons/clear.svg"
+          alt="sunny"
+          width={96}
+          height={96}
+        />
+      </div>
     </div>
   );
 }
