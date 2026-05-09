39bed884f3f1fa657c8c63dac34ea6f6329610da
Lin Lam <lin.lam@danfoss.com>
2026-05-09 22:10:50 +0300

    Init styles for AirConditions component

--- src/components/weather/air-conditions.tsx
+++ src/components/weather/air-conditions.tsx
@@ -1,14 +1,18 @@
+import s from "./air-conditions.module.scss";
 export function AirConditions() {
   return (
-    <article>
-      <div>
+    <article className={s.container}>
+      <div className="flex justify-space-between">
         <h2>Air Conditions</h2>
         <button>See more</button>
       </div>
 
-      <div>Wind</div>
-      <div>Chance of rain</div>
-      <div>Humidity</div>
+      <div className={s.conditions}>
+        <div>Wind</div>
+        <div>Chance of rain</div>
+        <div>Humidity</div>
+        <div>UV index</div>
+      </div>
     </article>
   );
 }
