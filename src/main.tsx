3cf7105446d5ca78a46b551ad26e43a9f4d3c51e
Lin Lam <lin.lam@danfoss.com>
2026-05-10 09:45:44 +0300

    Add TanStack Query setup instead of using the openmeteo for fetching
    React Query can take care of loading and error states etc.

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,17 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./styles/main.scss";
-import "./styles/flex-grid.scss";
 import App from "./App";
+
+import "./styles/flex-grid.scss";
+import "./styles/main.scss";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>,
 );
