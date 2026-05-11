75e5da416db688dd30a2873e9846ec8b57baca5f
Lin Lam <lin.lam@danfoss.com>
2026-05-11 12:17:39 +0300

    Import SVG images to React using vite-plugin-svgr
    - Refactor the WeatherIcons component to use the React SVG

--- src/components/weather/weather-icon.tsx
+++ src/components/weather/weather-icon.tsx
@@ -1,26 +1,11 @@
-function getWeatherIcon(code: number, isDay: boolean): string {
-  if (code === 0) {
-    return isDay
-      ? "src/assets/icons/clear.svg"
-      : "src/assets/icons/clear-night.svg";
-  }
-
-  if (code <= 3) {
-    return isDay
-      ? "src/assets/icons/partly-cloudy.svg"
-      : "src/assets/icons/cloudy.svg";
-  }
-
-  if (code === 0) return "src/assets/icons/clear.svg";
-  if (code <= 3) return "src/assets/icons/partly-cloudy.svg";
-  if (code <= 48) return "src/assets/icons/fog.svg";
-  if (code <= 67) return "src/assets/icons/rain.svg";
-  if (code <= 77) return "src/assets/icons/snow.svg";
-  if (code <= 82) return "src/assets/icons/rain.svg";
-  if (code <= 99) return "src/assets/icons/thunder.svg";
-
-  return "undefined";
-}
+import ClearSvg from "@/assets/icons/clear.svg?react";
+import ClearNight from "@/assets/icons/clear-night.svg?react";
+import PartlyCloudy from "@/assets/icons/partly-cloudy.svg?react";
+import Cloudy from "@/assets/icons/cloudy.svg?react";
+import Fog from "@/assets/icons/fog.svg?react";
+import Rain from "@/assets/icons/rain.svg?react";
+import Snow from "@/assets/icons/snow.svg?react";
+import Storm from "@/assets/icons/storm.svg?react";
 
 type Props = {
   code: number;
@@ -29,15 +14,21 @@ type Props = {
   height: number;
 };
 
-export function WeatherIcon({ code, isDay, width, height }: Props) {
-  const src = getWeatherIcon(code, isDay);
+function getWeatherIcon(code: number, isDay: boolean) {
+  if (code === 0) return isDay ? ClearSvg : ClearNight;
+  if (code <= 3) return isDay ? PartlyCloudy : Cloudy;
+  if (code <= 48) return Fog;
+  if (code <= 67) return Rain;
+  if (code <= 77) return Snow;
+  if (code <= 82) return Rain;
+  if (code <= 99) return Storm;
+  return "undefined";
+}
 
-  return (
-    <img
-      src={src}
-      alt="icon for showing the weather"
-      width={width}
-      height={height}
-    />
-  );
+export function WeatherIcon({ code, isDay, width, height }: Props) {
+  const Icon = getWeatherIcon(code, isDay) as React.ComponentType<{
+    width: number;
+    height: number;
+  }>;
+  return <Icon width={width} height={height} />;
 }
