248cac0edaa41822d88891464b0cbe3b3c18bb84
Lin Lam <lin.lam@danfoss.com>
2026-05-12 09:36:44 +0300

    Set up Vitest and React Testing Library

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import path from "path";
+import "vitest/config"; // <-- just dummy import
 
 export default defineConfig({
   base: "/weather/",
   plugins: [react(), svgr()],
+  test: {
+    globals: true,
+    setupFiles: "src/tests/test-setup.ts",
+    environment: "jsdom",
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
